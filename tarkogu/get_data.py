229f2768ed92f61b357d2e99ede2b42c070b8eab
kusurin <kusu_rin@sjtu.edu.cn>
2026-05-18 15:48:25 +0800

    完善价格预测以及买入卖出点判断、排序权重

--- tarkogu/get_data.py
+++ tarkogu/get_data.py
@@ -12,13 +12,19 @@ MS_IN_DAY = 1000 * 60 * 60 * 24
 CURRENT_DIR = get_pkg_abs_path()
 
 def get_item_metadata():
-    if os.path.exists(CURRENT_DIR / "data" / "item_metadata.csv"):
-        return pd.read_csv(CURRENT_DIR / "data" / "item_metadata.csv", index_col=0, header=0)
+    if os.path.exists(CURRENT_DIR / "data" / "items_metadata.csv"):
+        return pd.read_csv(CURRENT_DIR / "data" / "items_metadata.csv", index_col=0, header=0)
     query = """
     query {
         items(lang: en) {
             id
             name
+            category {
+                name
+                parent {
+                    name
+                }
+            }
         }
     }"""
 
@@ -26,6 +32,7 @@ def get_item_metadata():
     data = response.json()
 
     meta_df = pd.DataFrame(data['data']['items'])
+    meta_df['category'] = meta_df['category'].apply(lambda x: x['parent']['name'])
     meta_df.to_csv('data/items_metadata.csv', index=False, header=True)
     return meta_df
 
@@ -54,52 +61,83 @@ def get_item_historical_price(id, days, limit = INT_MAX, offset = 0):
 
 def fetch_item_historical_price(id, days):
     current_timestamp = pd.Timestamp.now(tz='UTC').timestamp() * 1000
+    
+    start_timestamp = current_timestamp - days * MS_IN_DAY - UPDATE_INTERVAL
 
     if not os.path.exists(CURRENT_DIR / "data" / f"{id}_historical_price.csv"):
         price_df = get_item_historical_price(id, days)
     else:
-        price_df = pd.read_csv(CURRENT_DIR / "data" / f"{id}_historical_price.csv", index_col=False, header=0)
-    
-        price_df_available = price_df[price_df['timestamp'] >= (pd.Timestamp.now().timestamp() * 1000 - days * MS_IN_DAY - UPDATE_INTERVAL)].copy()
-        if price_df_available.empty:
-            append_price_df = get_item_historical_price(id, days)
+        while True:
+            price_df = pd.read_csv(CURRENT_DIR / "data" / f"{id}_historical_price.csv", index_col=False, header=0)
 
-        else:
+            price_df_available = price_df[price_df['timestamp'] >= start_timestamp].copy()
+
+            if price_df_available.empty:
+                append_price_df = get_item_historical_price(id, days)
+                break
+
+            price_df_available_pad_start = pd.DataFrame([{
+                'timestamp': start_timestamp,
+                'price': price_df_available.iloc[0,:]['price'],
+                'priceMin': price_df_available.iloc[0,:]['priceMin'],
+                'offerCount': price_df_available.iloc[0,:]['offerCount'],
+                'time': pd.to_datetime(price_df_available.iloc[0,:]['timestamp'] - UPDATE_INTERVAL, unit='ms')
+            }])
+
+            price_df_available_pad_end = pd.DataFrame([{
+                'timestamp': current_timestamp,
+                'price': price_df_available.iloc[-1,:]['price'],
+                'priceMin': price_df_available.iloc[-1,:]['priceMin'],
+                'offerCount': price_df_available.iloc[-1,:]['offerCount'],
+                'time': pd.to_datetime(price_df_available.iloc[-1,:]['timestamp'] + UPDATE_INTERVAL, unit='ms')
+            }])
+
+            is_outdated = (current_timestamp - price_df_available.iloc[-1]['timestamp'] >= UPDATE_INTERVAL)
+
+            price_df_available = pd.concat([price_df_available_pad_start, price_df_available, price_df_available_pad_end], ignore_index=True)
+
             price_df_available['timestamp_diffs'] = price_df_available['timestamp'].diff()
-            price_df_available['should_fetch'] = price_df_available['timestamp_diffs'] >= UPDATE_INTERVAL * 2
-
-            is_outdated = current_timestamp - price_df_available.iloc[-1,:]['timestamp'] >= UPDATE_INTERVAL * 2
+            price_df_available['should_fetch'] = price_df_available['timestamp_diffs'] >= UPDATE_INTERVAL * 7
 
             append_price_df = pd.DataFrame()
 
-            if is_outdated or price_df_available['should_fetch'].any():
+            if price_df_available['should_fetch'].any():
                 
                 idx_should_fetch_first = price_df_available[price_df_available['should_fetch']].index[0]
                 idx_should_fetch_last = price_df_available[price_df_available['should_fetch']].index[-1]
 
-                fetch_time_start = price_df_available.iloc[idx_should_fetch_first - 1,:]['timestamp']
-                fetch_time_end = price_df_available.iloc[idx_should_fetch_last + 1,:]['timestamp']
-
-                if price_df_available.iloc[0,:]['timestamp'] - (current_timestamp - days * MS_IN_DAY) >= UPDATE_INTERVAL * 2:
-                    fetch_time_start = current_timestamp - days * MS_IN_DAY
-
                 if is_outdated:
-                    fetch_time_end = current_timestamp
+                    idx_should_fetch_last = price_df_available.index[-1]
 
-                # 不包含start和end
-                record_limit = (fetch_time_end - fetch_time_start) // (UPDATE_INTERVAL) - 1
+                fetch_time_start = price_df_available.iloc[idx_should_fetch_first - 1,:]['timestamp']
+                fetch_time_end = price_df_available.iloc[min(idx_should_fetch_last + 1, len(price_df_available) - 1),:]['timestamp']
 
-                offset_days = (current_timestamp - fetch_time_start) // (1000 * 60 * 60 * 24) + 1
-                offset_interval = (current_timestamp - fetch_time_start) // (UPDATE_INTERVAL) + 1
+            elif is_outdated:
+                fetch_time_start = price_df_available.iloc[-2,:]['timestamp']
+                fetch_time_end = price_df_available.iloc[-1,:]['timestamp']
+            
+            else:
+                print('Using cache')
+                break
 
-                request_offset = offset_days * (MS_IN_DAY / UPDATE_INTERVAL) - offset_interval
+            # 不包含start和end
+            record_limit = (fetch_time_end - fetch_time_start) // (UPDATE_INTERVAL) + 1
 
-                append_price_df = get_item_historical_price(id, offset_days, limit=record_limit, offset=request_offset)
+            offset_days = (current_timestamp - fetch_time_start) // (1000 * 60 * 60 * 24) + 1
+            offset_interval = (current_timestamp - fetch_time_start) // (UPDATE_INTERVAL) + 1
 
-        price_df = pd.concat([price_df, append_price_df], ignore_index=True)
+            request_offset = offset_days * (MS_IN_DAY / UPDATE_INTERVAL) - offset_interval
 
-        price_df.drop_duplicates('timestamp', inplace=True)
-        price_df.sort_values(by='timestamp', inplace=True)
+            append_price_df = get_item_historical_price(id, offset_days, limit=record_limit, offset=request_offset)
+                
+            price_df = pd.concat([price_df, append_price_df], ignore_index=True)
+
+            price_df.drop_duplicates('timestamp', inplace=True)
+            price_df.sort_values(by='timestamp', inplace=True)
+
+            break
+    
+    price_df['time'] = pd.to_datetime(price_df['time'], utc=True).dt.tz_convert(None)
 
     price_df.to_csv(CURRENT_DIR / "data" / f"{id}_historical_price.csv", index=False, header=True)
 
--- tarkogu/predict.py
+++ tarkogu/predict.py
@@ -1,26 +1,36 @@
 from tarkogu.get_data import fetch_item_historical_price
 import pandas as pd
+import numpy as np
 from prophet import Prophet
 
 def predict_price_and_offer_count(id, ref_days, predict_hours,  return_details=False):
     price_df = fetch_item_historical_price(id, ref_days)
 
-    price_df.rename(columns={'time': 'ds', 'price': 'y'}, inplace=True)
+    price_df.rename(columns={'time': 'ds', 'priceMin': 'y'}, inplace=True)
 
     offer_df = price_df[['ds', 'offerCount']].copy()
     offer_df.rename(columns={'offerCount': 'y'}, inplace=True)
 
-    # Predict price
-    price_model = Prophet(changepoint_prior_scale=0.01)
-    price_model.fit(price_df[['ds', 'y']])
-    price_future = price_model.make_future_dataframe(periods=predict_hours, freq='h')
-    price_forecast = price_model.predict(price_future)
-
-    # Predict offer count
-    offer_model = Prophet(changepoint_prior_scale=0.01)
+    offer_model = Prophet()
     offer_model.fit(offer_df[['ds', 'y']])
     offer_future = offer_model.make_future_dataframe(periods=predict_hours, freq='h')
     offer_forecast = offer_model.predict(offer_future)
+
+    # Predict price
+    price_model = Prophet()
+    price_model.add_regressor('offerCount')
+    price_model.fit(price_df[['ds', 'y', 'offerCount']])
+    price_future = price_model.make_future_dataframe(periods=predict_hours, freq='h')
+    offer_regressor = offer_forecast[['ds', 'yhat']].rename(columns={'yhat': 'offerCount'})
+    price_future = price_future.merge(offer_regressor, on='ds', how='left')
+
+    if price_future['offerCount'].isnull().any():
+        fallback = price_df[['ds', 'offerCount']]
+        price_future = price_future.merge(fallback, on='ds', how='left', suffixes=('', '_hist'))
+        price_future['offerCount'] = price_future['offerCount'].fillna(price_future['offerCount_hist'])
+        price_future.drop(columns=['offerCount_hist'], inplace=True)
+
+    price_forecast = price_model.predict(price_future)
 
     if return_details:
         return price_forecast, offer_forecast, price_model, offer_model
@@ -30,4 +40,107 @@ def predict_price_and_offer_count(id, ref_days, predict_hours,  return_details=F
 
     result_df = pd.concat([price_forecast, offer_forecast], axis=1)
 
+    result = pd.merge(price_df, result_df, left_on='ds', right_on='price_ds', how='inner')
+    result = result.dropna(subset=['y', 'price_yhat'])
+    mse = np.mean((result['y'] - result['price_yhat']) ** 2)
+    rmse = np.sqrt(mse)
+
+    return result_df, rmse
+
+def predict_price_and_offer_count_test(id, ref_days, predict_hours,  return_details=False, train_fraction=0.8):
+    price_df = fetch_item_historical_price(id, ref_days)
+
+    price_df.rename(columns={'time': 'ds', 'priceMin': 'y'}, inplace=True)
+
+    offer_df = price_df[['ds', 'offerCount', 'timestamp']].copy()
+    offer_df.rename(columns={'offerCount': 'y'}, inplace=True)
+
+    train_test_split_time = price_df['timestamp'].min() + (price_df['timestamp'].max() - price_df['timestamp'].min()) * train_fraction
+    
+    price_df_train = price_df[price_df['timestamp'] < train_test_split_time].copy()
+    price_df_test = price_df[price_df['timestamp'] >= train_test_split_time].copy()
+
+    offer_df_train = offer_df[offer_df['timestamp'] < train_test_split_time].copy()
+    offer_df_test = offer_df[offer_df['timestamp'] >= train_test_split_time].copy()
+
+    offer_model = Prophet()
+    offer_model.fit(offer_df_train[['ds', 'y']])
+    offer_future = offer_model.make_future_dataframe(periods=predict_hours, freq='h')
+    offer_forecast = offer_model.predict(offer_future)
+
+    # Predict price
+    price_model = Prophet()
+    price_model.add_regressor('offerCount')
+    price_model.fit(price_df_train[['ds', 'y', 'offerCount']])
+    price_future = price_model.make_future_dataframe(periods=predict_hours, freq='h')
+    offer_regressor = offer_forecast[['ds', 'yhat']].rename(columns={'yhat': 'offerCount'})
+    price_future = price_future.merge(offer_regressor, on='ds', how='left')
+
+    if price_future['offerCount'].isnull().any():
+        fallback = price_df_train[['ds', 'offerCount']]
+        price_future = price_future.merge(fallback, on='ds', how='left', suffixes=('', '_hist'))
+        price_future['offerCount'] = price_future['offerCount'].fillna(price_future['offerCount_hist'])
+        price_future.drop(columns=['offerCount_hist'], inplace=True)
+
+    price_forecast = price_model.predict(price_future)
+
+    if return_details:
+        return price_forecast, offer_forecast, price_model, offer_model, price_df_train, price_df_test, offer_df_train, offer_df_test
+    
+    price_forecast.columns = ['price_' + _col for _col in price_forecast.columns]
+    offer_forecast.columns = ['offer_' + _col for _col in offer_forecast.columns]
+
+    result_df = pd.concat([price_forecast, offer_forecast], axis=1)
+
     return result_df
+
+# 当时价格和预测价格中，后续价格大于其超过10000的时间点买入
+def find_buy_times(price_forecast):
+    current_time = pd.Timestamp.now(tz='UTC').tz_localize(None)
+    last_idx = price_forecast[price_forecast['price_ds'] <= current_time].index[-1]
+
+    price_forecast = price_forecast.iloc[last_idx:][::-1].copy()
+    price_forecast.reset_index(drop=True, inplace=True)
+
+    max_follow = float('-inf')
+    
+    price_forecast['max_follow_diff'] = 0.0
+
+    for idx, row in price_forecast.iterrows():
+        price_forecast.loc[idx, 'max_follow_diff'] = max_follow - row['price_yhat']
+        max_follow = max(max_follow, row['price_yhat'])
+    
+    price_forecast['should_buy'] = price_forecast['max_follow_diff'] >= 10000
+
+    range = price_forecast['max_follow_diff'].max()
+
+    return price_forecast[price_forecast['should_buy']]['price_ds'].tolist(), range
+
+
+# 当时价格和预测价格中，前24h内有价格小于其超过10000的时间点卖出
+def find_sell_times(price_forecast, previous_ref_hours=24):
+    current_time = pd.Timestamp.now(tz='UTC').tz_localize(None)
+
+    window_start = current_time - pd.Timedelta(hours=previous_ref_hours)
+    price_forecast = price_forecast[price_forecast['price_ds'] >= window_start].copy()
+    price_forecast.reset_index(drop=True, inplace=True)
+
+    if price_forecast.empty:
+        return []
+
+    last_idx = price_forecast[price_forecast['price_ds'] <= current_time].index[-1]
+    if last_idx <= 0:
+        return []
+
+    min_previous = price_forecast.loc[:last_idx, 'price_yhat'].min()
+    price_forecast['max_previous_diff'] = 0.0
+
+    for idx in range(last_idx, len(price_forecast)):
+        price_forecast.loc[idx, 'max_previous_diff'] = price_forecast.loc[idx, 'price_yhat'] - min_previous
+        min_previous = min(min_previous, price_forecast.loc[idx, 'price_yhat'])
+
+    price_forecast['should_sell'] = price_forecast['max_previous_diff'] >= 10000
+
+    range = price_forecast['max_previous_diff'].max()
+
+    return price_forecast[price_forecast['should_sell']]['price_ds'].tolist(), range
